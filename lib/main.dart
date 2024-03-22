57886ceee6f1eccff8e0944d1c04837842f48758
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-03-22 09:11:31 +0100

    Configure Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:auth_locale/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
