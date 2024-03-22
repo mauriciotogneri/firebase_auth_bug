a9068a4b4ea1a28f7a84e36aab7d0cac3a75193a
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-03-22 10:20:02 +0100

    Improve functions

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,5 +1,13 @@
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions/v1';
 import {beforeUserCreated} from 'firebase-functions/v2/identity';
 
+admin.initializeApp();
+
+export const authUserCreated = functions.auth.user().onCreate(async (user) => {
+  console.log('authUserCreated', JSON.stringify(user));
+});
+
 export const authBeforeUserCreated = beforeUserCreated((event) => {
-  console.log(JSON.stringify(event));
+  console.log('authBeforeUserCreated', JSON.stringify(event));
 });
